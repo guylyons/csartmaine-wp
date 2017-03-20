d36395a6c77d7de83d790bd4d7e6adcdf6f9a169
Guy Lyons <guylyons2@gmail.com>
2017-03-19 20:51:36 -0400

    linting

--- frontend/fluff.js
+++ frontend/fluff.js
@@ -5,30 +5,30 @@ jQuery(document).ready(function($) {
     dots: true,
     prevArrow: '<i class=\'material-icons slick-prev\'>chevron_left</i>',
     nextArrow: '<i class=\'material-icons slick-next\'>chevron_right</i>'
-  });
-
+  })
+  
   $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
+    var scroll = $(window).scrollTop()
     if (scroll >= 96) {
-      $('.nav-primary-scroll').addClass('nav-primary-scroll--toggle');
+      $('.nav-primary-scroll').addClass('nav-primary-scroll--toggle')
     } else {
-      $('.nav-primary-scroll').removeClass('nav-primary-scroll--toggle');
+      $('.nav-primary-scroll').removeClass('nav-primary-scroll--toggle')
     }
-  });
+  })
 
   // Smooth Scrolling
   $(function() {
     $('a[href*=\'#\']:not([href=\'#\'])').click(function() {
       if (window.location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && window.location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        var target = $(this.hash)
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']')
         if (target.length) {
           $('html, body').animate({
             scrollTop: target.offset().top
-          }, 1000);
-          return false;
+          }, 1000)
+          return false
         }
       }
-    });
-  });
-});
+    })
+  })
+})
