058fd90f15893234f718e186a8ee3b866a4d6d7a
Guy Lyons <guylyons2@gmail.com>
2017-03-21 08:38:58 -0400

    overlays for blog index

--- template-parts/blog/hero.php
+++ template-parts/blog/hero.php
@@ -1,12 +1,12 @@
-<?php $hero_url = get_the_post_thumbnail_url( $post->ID ); ?>
+<?php $hero_url = get_the_post_thumbnail_url( $post->ID, 'full' ); ?>
 <?php if( $hero_url ): ?>
     </section>
         <div class="hero-image" style="background-image: url('<?php echo $hero_url; ?>');">
             <!-- image-url: <?php echo $hero_url; ?> -->
-            <header  class="post-header">
+            <header class="post-header">
                 <?php $subhead = get_field( 'subhead' ); ?>
                 <div class="post-meta"><?php if( $cat ) { echo $cat->name . ' | '; } ?><?php the_author(); ?> | <time><?php the_date( 'd F Y' ); ?></time></div>
-                <h1 class="post-title"><?php the_title(); ?></h1>
+                <h1 class="post-title black-box"><?php the_title(); ?></h1>
                 <?php if( $subhead ) { echo '<h5 class="post-subhead">' . $subhead . '</h5>'; } ?>
             </header>
         </div>
--- template-parts/blog/index.php
+++ template-parts/blog/index.php
@@ -1,25 +1,26 @@
 <?php
 /**
- * The template for displaying link post formats
- *
- * Used for both single and index/archive/search.
- *
- */
+* The template for displaying link post formats
+*
+* Used for both single and index/archive/search.
+*
+*/
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'index-item' ); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class('index-item'); ?>>
 
 	<section class="entry-content">
 
-        <a href="<?php the_permalink(); ?>" class="index-content">
-			<?php $hero_url = get_the_post_thumbnail_url( $post->ID, 'index_blog' ); ?>
-            <section class="index-details"  style="background-image: url('<?php echo $hero_url; ?>');">
-            	<time class="post-date"><?php the_date( 'd F, Y' ); ?></time>
-	                <h2 class="index-title"><?php the_title(); ?></h2>
-	                <div class="index-author">By <?php the_author(); ?></div>
-            	<div class="index-excerpt"><?php the_excerpt(); ?></div>
-            </section>
-        </a>
+		<a href="<?php the_permalink(); ?>" class="index-content">
+			<?php $hero_url = get_the_post_thumbnail_url($post->ID, 'index_blog'); ?>
+			<section class="index-details"  style="background-image: url('<?php echo $hero_url; ?>');">
+				<time class="post-date"><?php the_date('d F, Y'); ?></time>
+				<h2 class="index-title"><?php the_title(); ?></h2>
+				<div class="index-author">By <?php the_author(); ?></div>
+				<div class="index-excerpt"><?php the_excerpt(); ?></div>
+				<div class="overlay"></div>
+			</section>
+		</a>
 
 	</section>
 
