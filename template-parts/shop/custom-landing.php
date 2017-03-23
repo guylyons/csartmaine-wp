7a1f4bfdfe8c2f4e01f978bcc7043575b995b6e9
Guy Lyons <guylyons2@gmail.com>
2017-03-22 23:50:20 -0400

    remove placeholders

--- template-parts/shop/custom-landing.php
+++ template-parts/shop/custom-landing.php
@@ -10,7 +10,7 @@ if(is_shop() && $feature_a || $feature_b):
     <ul class="products">
         <li class="col2 product featured">
             <a href="<?php the_permalink($feature_a->ID); ?>" class="woocommerce-LoopProduct-link">
-                <img src="//placehold.it/800x300" title="" alt="">
+                <?php echo get_the_post_thumbnail( $feature_a->ID ); ?>
                 <h3><?php echo $feature_a->post_title; ?></h3>
                 <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">
                     $</span><?php echo $_product->get_price(); ?></span></span>
@@ -19,7 +19,7 @@ if(is_shop() && $feature_a || $feature_b):
         <?php $_product = wc_get_product( $feature_b->ID ); ?>
         <li class="col2 product featured">
             <a href="<?php the_permalink($feature_b->ID); ?>" class="woocommerce-LoopProduct-link">
-                <img src="//placehold.it/800x300" title="" alt="">
+                <?php echo get_the_post_thumbnail( $feature_b->ID ); ?>
                 <h3><?php echo $feature_b->post_title; ?></h3>
                 <span class="price"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">
                     $</span><?php echo $_product->get_price(); ?></span></span>
