23068e983fbc168815a129fee7ec39575e0a485d
Guy Lyons <guylyons2@gmail.com>
2018-03-09 09:50:14 -0500

    start grid

--- front-page.php
+++ front-page.php
@@ -11,6 +11,35 @@ get_header();
 
 <main role="main" class="main">
 
+    <section>
+        <div class="grid">
+            <div class="item">
+                <h2>Grid Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquam quisquam, reiciendis officiis dolorem architecto ab minus odit. Quaerat dolorum, consequatur inventore est minima culpa rerum accusamus reiciendis perferendis iste!</p>
+            </div>
+            <div class="item">
+                <h2>Grid Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquam quisquam, reiciendis officiis dolorem architecto ab minus odit. Quaerat dolorum, consequatur inventore est minima culpa rerum accusamus reiciendis perferendis iste!</p>
+            </div>
+            <div class="item">
+                <h2>Grid Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquam quisquam, reiciendis officiis dolorem architecto ab minus odit. Quaerat dolorum, consequatur inventore est minima culpa rerum accusamus reiciendis perferendis iste!</p>
+            </div>
+             <div class="item">
+                <h2>Grid Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquam quisquam, reiciendis officiis dolorem architecto ab minus odit. Quaerat dolorum, consequatur inventore est minima culpa rerum accusamus reiciendis perferendis iste!</p>
+            </div>
+            <div class="item">
+                <h2>Grid Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquam quisquam, reiciendis officiis dolorem architecto ab minus odit. Quaerat dolorum, consequatur inventore est minima culpa rerum accusamus reiciendis perferendis iste!</p>
+            </div>
+            <div class="item">
+                <h2>Grid Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia aliquam quisquam, reiciendis officiis dolorem architecto ab minus odit. Quaerat dolorum, consequatur inventore est minima culpa rerum accusamus reiciendis perferendis iste!</p>
+            </div>
+        </div>
+    </section>
+
     <?php get_template_part( 'template-parts/slider' ); ?>
 
     <section class="introduction">
@@ -21,6 +50,8 @@ get_header();
 
     <?php get_template_part( 'template-parts/meet-the-artist' ); ?>
     <?php get_template_part( 'template-parts/recent-products' ); ?>
+    <?php get_template_part( 'template-parts/newsletter-signup' ); ?>
+
 </main>
 
 <?php get_footer(); ?>
