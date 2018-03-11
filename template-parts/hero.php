b1dc9c580d583655503b39f297b81f809b5783af
Guy Lyons <guylyons2@gmail.com>
2018-03-11 19:46:48 -0400

    shop, cats

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -3,33 +3,33 @@
  * Hero Template
  *
  */
-$index_id = get_option( 'page_for_posts' );
-$shop_id  = get_option( 'woocommerce_shop_page_id' );
+$index_id = get_option('page_for_posts');
+$shop_id  = get_option('woocommerce_shop_page_id');
 $hero_title = '';
 $hero_banner = '';
 $hero_url = '';
 
-if( is_blog() ) {
-    $hero_banner = get_field( 'hero_banner', $index_id );
-    $hero_title  = get_the_title( $index_id );
-} else if ( is_archive() && !is_shop() ) {
-    $hero_banner = get_field( 'hero_banner', 145 );
-    $hero_title  = get_the_title( 145 );
-} else if ( is_shop() || is_product() || is_product_category()  ) {
-    $hero_banner = get_field( 'hero_banner', $shop_id );
-    $hero_title  = get_the_title( $shop_id );
+if (is_blog()) {
+    $hero_banner = get_field('hero_banner', $index_id);
+    $hero_title  = get_the_title($index_id);
+} elseif (is_archive() && !is_shop()) {
+    $hero_banner = get_field('hero_banner', 145);
+    $hero_title  = get_the_title(145);
+} elseif (is_shop() || is_product() || is_product_category()) {
+    $hero_banner = get_field('hero_banner', $shop_id);
+    $hero_title  = get_the_title($shop_id);
 } else {
-    $hero_banner = get_field( 'hero_banner', $post->ID );
-    $hero_title = get_the_title( $post->ID );
+    $hero_banner = get_field('hero_banner', $post->ID);
+    $hero_title = get_the_title($post->ID);
 }
 
-if ( $hero_banner != 0 ) {
+if ($hero_banner != 0) {
     $hero_url = $hero_banner['sizes']['hero'];
 }
 
 ?>
 
-<?php if( $hero_url ): ?>
+<?php if ($hero_url && is_single('post')) : ?>
     <header>
         <div class="hero-image" style="background-image: url('<?php echo $hero_url; ?>');">
             <div class="header">
