91dde64ba13c0968995192847c7a8f859c5c77ad
Guy Lyons <guylyons2@gmail.com>
2018-04-17 13:42:55 -0400

    remove

--- template-parts/instagram.php
+++
@@ -1,5 +0,0 @@
-<section class="instagram-feed center">
-    <h2>Be a part of the local conversartion.</h2>
-    <h3><strong>#CSArtMaine</strong></h3>
-    <?php echo do_shortcode('[instashow id="1"]'); ?>
-</section>
